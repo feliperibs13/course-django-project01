b1a783d353c6748dcf259e5e006244b5c713f9a8
Felipe Ribeiro <felipeush@gmail.com>
2022-06-25 21:09:48 -0300

    creating namespace to avoid name collision

--- projeto/settings.py
+++ projeto/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Informando ao DJANGO o/os aplicativos criados.
+    'recipes'
 ]
 
 MIDDLEWARE = [
